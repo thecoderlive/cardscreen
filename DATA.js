c3db47335bf0bc765ac52daca5e25ced671657f2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-19 15:54:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,51 @@
+export const item = {
+    "id": "1c423f3a-2958-4927-9c5d-1ccfcd68cc4b",
+    "main_title": "my",
+    "bank_name": "employee",
+    "bank_name_tilted": "little",
+    "card_number": "kitchen",
+    "card_holder": "trip",
+    "card_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/6.png",
+    "statements": [
+        {
+            "id": "ca87e525-2f9d-4d86-9b5f-554b82b45be4",
+            "date": "at",
+            "month": "message",
+            "price": "consumer",
+            "top_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png",
+            "about_statement": "main"
+        },
+        {
+            "id": "310a46ea-0a6c-40b2-a9ea-fd832ef203b4",
+            "date": "cold",
+            "month": "left",
+            "price": "sister",
+            "top_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png",
+            "about_statement": "seat"
+        },
+        {
+            "id": "eef3f5dd-3e6e-4866-8aa8-30581e864c1a",
+            "date": "item",
+            "month": "start",
+            "price": "although",
+            "top_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png",
+            "about_statement": "answer"
+        },
+        {
+            "id": "8efb02ec-f4f2-435a-9510-c9b2ea6b9175",
+            "date": "floor",
+            "month": "child",
+            "price": "play",
+            "top_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png",
+            "about_statement": "employee"
+        },
+        {
+            "id": "86263447-b423-4957-894f-0b3191394be2",
+            "date": "large",
+            "month": "fear",
+            "price": "between",
+            "top_arrow": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png",
+            "about_statement": "hair"
+        }
+    ]
+}
